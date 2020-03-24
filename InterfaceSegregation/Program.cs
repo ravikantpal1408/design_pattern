413c8c98180278e6d9cdcb73dc8bba2573b4ce21
ravikantpal1408 <ravikantpal123@gmail.com>
2020-03-24 12:13:38 +0530

    done with interface segregation principal

---
+++ InterfaceSegregation/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace InterfaceSegregation
+{
+    public class Document
+    {
+        
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionPrinter : IPrinter, IScan
+    {
+        public void Print(Document d)
+        {
+            // 
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFashionedPrinter
+    {
+        
+    }
+    
+    // instead of having one big IMachine interface one can chunk the functionality into different printer 
+    public interface IPrinter
+    {
+        // contains Printing contracts
+        void Print(Document d);
+    }
+
+    public interface IScan
+    {
+        // contains Scanning contracts
+        void Scan(Document d);
+    }
+    
+    // now suppose there is a Photocopier class we can implement both the interface face into that class
+
+    public class Photocopier: IPrinter, IScan
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* If interface is getting populated then you should chunk into pieces */
+            
+            Console.WriteLine("Interface Segregation Principal !!");
+        }
+        
+        
+    }
+}
