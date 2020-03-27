74eb0d78e020a516e6e10c776730f5373edb6a65
ravikantpal1408 <ravikantpal123@gmail.com>
2020-03-27 11:46:03 +0530

    Fluent builder complete

---
+++ Builder/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder {
+    public class HtmlElement {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement> ();
+        private const int indentSize = 2;
+
+        public HtmlElement () { }
+
+        public HtmlElement (string name, string text) {
+            Name = name ??
+                throw new ArgumentNullException (paramName: nameof (name));
+            Text = text ??
+                throw new ArgumentNullException (paramName: nameof (text));
+        }
+
+        private string ToStringImpl (int indent) {
+            var sb = new StringBuilder ();
+            var i = new string (' ', indentSize * indent);
+            sb.Append ($"{i}<{Name}>");
+            if (!string.IsNullOrWhiteSpace (Text)) {
+                sb.Append (new string (' ', indentSize * (indent + 1)));
+                sb.AppendLine (Text);
+            }
+
+            foreach (var element in Elements) {
+                sb.Append (element.ToStringImpl (indent + 1));
+            }
+
+            sb.Append ($"{i}</{Name}>");
+
+            return sb.ToString ();
+        }
+
+        public override string ToString () {
+            return ToStringImpl (0);
+        }
+    }
+
+    public class HtmlBuilder {
+        private readonly string _rootName;
+        HtmlElement root = new HtmlElement ();
+
+        public HtmlBuilder (string rootName) {
+            _rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public void AddChild (string childName, string childText) {
+            var e = new HtmlElement (childName, childText);
+            root.Elements.Add (e);
+        }
+
+        public override string ToString () {
+            return root.ToString ();
+        }
+
+        public void Clear () {
+            root = new HtmlElement { Name = _rootName };
+        }
+
+    }
+
+    class Program {
+        /* Builder creation design pattern */
+
+        // Motivation :
+        // 1- Some object are simple and can be created in a single constructor call
+        // 2- Other object require a lot of ceremony to create
+        // 3- HaHaving an object 10 constructor argument is not productive
+        // 4- Instead, opt for piecewise construction
+        // 5- Builder provides an API for constructing an object step by step
+        static void Main (string[] args) {
+            var hello = "hello";
+            var sb = new StringBuilder ();
+            sb.Append ("<p>");
+            sb.Append (hello);
+            sb.Append ("</p>");
+            Console.WriteLine (sb); // simple example of builder pattern
+
+            var words = new [] { "Hello", "World" };
+            sb.Clear ();
+            sb.Append ("<ul>");
+            foreach (var word in words) {
+                sb.AppendFormat ("<li>{0}</li>", word);
+            }
+
+            sb.Append ("</ul>");
+
+            Console.WriteLine (sb);
+
+            var builder = new HtmlBuilder ("ul");
+            builder.AddChild ("li", "hello");
+            builder.AddChild ("li", "world");
+
+            Console.WriteLine (builder.ToString ());
+
+            Console.ReadKey ();
+        }
+    }
+}
---
+++ FluentBuilder/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace FluentBuilder
+{
+    /*
+     * Access Modifiers
+     * 
+     * public              - The type or member can be accessed by any other code in the same assembly or another assembly that references it.
+     * 
+     * private             - The type or member can be accessed only by code in the same class or struct
+     * 
+     * protected           - The type or member can be accessed only by code in the same class, or in a class that is derived from that class
+     * 
+     * internal            - The type or member can be accessed by any code in the same assembly, but not from another assembly.
+     * 
+     * protected internal  - The type or member can be accessed by any code in the assembly in which it's declared,
+     *                       or from within a derived class in another assembly
+     * 
+     * private protected   - The type or member can be accessed only within its declaring assembly,
+     *                       by code in the same class or in a type that is derived from that class.
+     * 
+     */
+
+    public class Person
+    {
+        public string Name;
+        public string Position;
+
+        public class Builder : PersonJobBuilder<Builder>
+        {
+            
+        }
+        
+        public static Builder New => new Builder();
+        
+        
+        public override string ToString()
+        {
+            return $"{nameof(Name)} : {Name} , {nameof(Position)} : {Position}";
+        }
+
+    }
+
+
+    public abstract class PersonBuilder
+    {
+        // we are using protected because we will implement inheritance 
+        protected Person _person = new Person();
+
+        public Person Build()
+        {
+            return _person;
+        }
+    }
+
+    // Now creating person info builder 
+    public class PersonInfoBuilder<SELF> : PersonBuilder where SELF : PersonInfoBuilder<SELF>
+    {
+        // Fluent method 
+        public SELF Called(string name)
+        {
+            _person.Name = name;
+            return (SELF) this;
+        }
+    }
+
+
+    public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>> where SELF: PersonJobBuilder<SELF>
+    {
+        public SELF WorkAsA(string position)
+        {
+            _person.Position = position;
+            return (SELF) this;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var me = Person.New.Called("Ravi Kant Pal").WorkAsA("Consultant").Build();
+            
+            Console.WriteLine($"{me} , 😃");
+        }
+    }
+}
+
